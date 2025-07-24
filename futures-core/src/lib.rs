3db5d2b14c939cc5d9d1060212ea41514c9805b3
Asa Paparo <asapaparo@gmail.com>
2025-07-23 23:33:21 -0400

    cleanup

--- futures-core/src/lib.rs
+++ futures-core/src/lib.rs
@@ -1,20 +1,9 @@
-use std::{
-    cell::UnsafeCell,
-    marker::PhantomData,
-    mem,
-    pin::Pin,
-    task::{Context, Poll, RawWaker, Waker},
-};
+use std::task::Poll;
 
-pub mod compat;
+mod compat;
+mod task;
 
-/// A task that can be woken.
-///
-/// This acts as a handle for a reactor to indicate when a `ScopedFuture` is
-/// once again ready to be polled.
-pub trait Wake<'scope> {
-    fn wake(&self);
-}
+pub use crate::task::Wake;
 
 /// ScopedFuture represents a unit of asynchronous computation that must be
 /// polled by an external actor.
